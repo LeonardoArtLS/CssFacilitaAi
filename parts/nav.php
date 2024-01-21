8ddfd3087b28449ee1935511e8b943fe9abd839b
Leonardo Arthur <leonardoarthu627@gmail.com>
2024-01-21 09:30:09 -0300

    nav

---
+++ parts/nav.php
@@ -0,0 +1,43 @@
+
+<nav class="navbar m-0 p-0 navbar-expand-md">
+        <div class="container-fluid m-0 p-0">
+
+        <div class="col-md-3">
+            <a class="navbar-brand text-light ml-0 p-0" href="#">
+                <img src="img/facilitaai.png" alt="Logo" width="60" height="60" class="align-text-center">
+                FacilitaAí
+            </a>
+        </div>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav col-md-10 p-0 m-0">
+
+                    <div class="p-0 m-0 col-md-8 col-lg-7 d-md-flex justify-content-end">
+                        <li class="nav-item d-md-inline-flex justify-content-center align-items-center navitem">
+                            <a class="nav-link text-center text-light pt-md-2 navlink ativolink" href="#">Quero contratar</a>
+                        </li>
+                        <li class="nav-item d-md-inline-flex justify-content-center align-items-center navitem">
+                            <a class="nav-link text-center text-light pt-md-2 navlink" href="#">Quero trabalhar</a>
+                        </li>
+                    </div>
+
+                    <div class="col-md-6 col-lg-7 d-md-flex justify-content-end">
+                        <li class="nav-item d-md-inline-flex justify-content-center align-items-center hover rounded-5 px-2">
+                            <a class="nav-link text-center text-light" href="#">Faça login</a>
+                        </li>
+                        <li class="nav-item d-md-inline-flex justify-content-center align-items-center">
+                            <a class="nav-link text-center bg-white
+                            text-dark rounded-5" href="#">Cadastre-se</a>
+                        </li>
+                    </div>
+                </ul>
+                <div class="nav-item pb-2"></div>
+            </div>
+
+        </div>
+    </nav>
